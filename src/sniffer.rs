4906c3a9b25d722905f85671ef5e93344877cabb
Giuliano Bellini s294739 <s294739@studenti.polito.it>
2023-09-25 11:59:55 +0200

    reformat packet info

--- src/sniffer.rs
+++ src/sniffer.rs
@@ -9,40 +9,36 @@ pub enum PacketDirection {
 }
 
 pub fn print_packet_info(pkt_data: &[u8], direction: PacketDirection) {
-    // packet directionality
-    debug!("{:?} packet sniffed!", direction);
     // total size (headers + payload)
-    debug!("    - Total size: {} B", pkt_data.len());
+    let size = pkt_data.len();
     if let Ok(headers) = PacketHeaders::from_ethernet_slice(pkt_data) {
         // payload
-        debug!("    - Payload size: {}", headers.payload.len());
-        debug!("    - Payload content: {}", String::from_utf8_lossy(headers.payload).into_owned());
-
+        let payload = String::from_utf8_lossy(headers.payload).into_owned();
         // ip layer
-        if let Some(ip) = headers.ip {
-            let ip_layer = match ip {
+        let ip_layer = if let Some(ip) = headers.ip {
+            match ip {
                 IpHeader::Version4(_, _) => {"IPv4"}
                 IpHeader::Version6(_, _) => {"IPv6"}
-            };
-            debug!("    - IP version: {ip_layer}");
+            }
         } else {
-            debug!("    - Cannot parse packet's IP header")
-        }
-
+            "////"
+        };
         // transport layer
-        if let Some(transport) = headers.transport {
-            let transport_layer = match transport {
+        let transport_layer = if let Some(transport) = headers.transport {
+            match transport {
                 TransportHeader::Udp(_) => {"UDP"}
                 TransportHeader::Tcp(_) => {"TCP"}
                 TransportHeader::Icmpv4(_) => {"ICMPv4"}
                 TransportHeader::Icmpv6(_) => {"ICMPv6"}
-            };
-            debug!("    - Transport protocol {transport_layer}");
+            }
         } else {
-            debug!("    - Cannot parse packet's transport header")
-        }
-
+            "////"
+        };
+        println!("{}","-".repeat(42));
+        println!("{:?} packet: {:^6}B | {:^6} | {:^6}", direction, size, ip_layer, transport_layer);
+        println!("Payload: {}", payload);
+        println!("{}","-".repeat(42));
     } else {
-        debug!("    - Cannot parse packet's headers...");
+        debug!("Cannot parse packet's headers...");
     }
 }
