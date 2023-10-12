08fb7a0a53a557af5c692d4185b5efc6d12a4d42
Giuliano Bellini s294739 <s294739@studenti.polito.it>
2023-10-12 14:59:07 +0200

    minor fixes

--- examples/forwarder.rs
+++ examples/forwarder.rs
@@ -96,7 +96,7 @@ fn transmit(pci_addr: String) {
                 p[i] = *data;
             }
             // add something different to each payload to distinguish this packet
-            p[49] = format!("{:b}", id).parse().unwrap();
+            p[49] = id;
             id = id.wrapping_add(1);
             let checksum = calc_ipv4_checksum(&p[14..14 + 20]);
             // Calculated checksum is little-endian; checksum field is big-endian
